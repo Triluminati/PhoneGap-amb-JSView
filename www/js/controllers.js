a2d8edcabc8d8de1c19918caca5a2db26c4b19cc
Triluminati <msantos11@iesnumancia.cat>
2016-01-14 11:51:21 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,15 +3,18 @@
 */
 
 $JSView.controller = {
-    sol: function(e){
+    inici: function(e){
 		$JSView.dataView({},e)
 	},
-    mercuri: function(e){
+    sol: function(e){
 		$JSView.dataView({},e)	
+	},
+    mercuri: function(e){
+		$JSView.dataView({},e)
 	},
     venus: function(e){
 		$JSView.dataView({},e)
-	},
+	},	
     modalA: function(e){
 		$JSView.dataView({},e)	
 	}
